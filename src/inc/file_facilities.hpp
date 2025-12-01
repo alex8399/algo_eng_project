df963ebd76f013f38bd3922c1d81dd5faddb27af
Alexander Vardanyan <vardart01@gmail.com>
2025-12-01 11:59:33 +0100

    Add references

--- src/inc/file_facilities.hpp
+++ src/inc/file_facilities.hpp
@@ -8,11 +8,11 @@
 
 namespace FileFacilities
 {
-    void read_graph(const std::string graph_file, CHGraph::Graph graph);
+    void read_graph(const std::string &graph_file, CHGraph::Graph &graph);
 
-    void read_destinations(const std::string destinations_file, std::vector<CHGraph::Destination> destinations);
+    void read_destinations(const std::string &destinations_file, std::vector<CHGraph::Destination> &destinations);
 
-    void dump_measurement(const Measurement measurement, const std::string output_file);
+    void dump_measurement(const Measurement &measurement, const std::string &output_file);
 }
 
 #endif
--- src/lib/file_facilities.cpp
+++ src/lib/file_facilities.cpp
@@ -1,13 +1,13 @@
 #include "file_facilities.hpp"
 
-void FileFacilities::read_graph(const std::string graph_file, CHGraph::Graph graph)
+void FileFacilities::read_graph(const std::string &graph_file, CHGraph::Graph &graph)
 {
 }
 
-void FileFacilities::read_destinations(const std::string destinations_file, std::vector<CHGraph::Destination> destinations)
+void FileFacilities::read_destinations(const std::string &destinations_file, std::vector<CHGraph::Destination> &destinations)
 {
 }
 
-void FileFacilities::dump_measurement(const Measurement measurement, const std::string output_file)
+void FileFacilities::dump_measurement(const Measurement &measurement, const std::string &output_file)
 {
 }
