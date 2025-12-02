b748cfb41750dbbe2f444ab65ea69073df79c54d
max-geo <skair0.rs@gmail.com>
2025-12-02 21:28:05 +0100

    definitions for graphs, routes etc.

--- src/inc/ch_graph.hpp
+++ src/inc/ch_graph.hpp
@@ -1,23 +1,43 @@
 #ifndef __GRAPH_HPP__
 #define __GRAPH_HPP__
 
+#include <vector>
+
 namespace CHGraph
 {
 
     struct Graph
     {
+        std::vector<int> from;
+        std::vector<int> to;
+        std::vector<double> weights;
+
+        int num_nodes = 0;
     };
 
     struct PreprocGraph
     {
+        std::vector<int> ranks;
+        
+        struct Arc {
+            int from;
+            int to;
+            double weight;
+            int mid_node;
+        };
+        std::vector<Arc> arcs;
     };
 
     struct Route
     {
+        double total_weight = 0;
+        std::vector<int> nodes;
     };
 
     struct Destination
     {
+        int source = -1;
+        int target = -1;
     };
 
     void preproc_graph_bottom_up(const Graph &graph, PreprocGraph &preproc_graph);
