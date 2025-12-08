4aa212d30a1b20b9190fdb6766ba91c4ac8ee022
Alexander Vardanyan <vardart01@gmail.com>
2025-12-08 22:24:57 +0100

    Include file_failities.hpp into test file

--- src/tst/test_file_facilities.cpp
+++ src/tst/test_file_facilities.cpp
@@ -1,11 +1,11 @@
 #include <gtest/gtest.h>
-#include "ch_graph.hpp"
+#include "file_facilities.hpp"
+#include <string>
 
 TEST(TestCaategory2, Test2) {
     CHGraph::Graph graph;
-    CHGraph::PreprocGraph preproc_graph;
-    CHGraph::preproc_graph_bottom_up(graph, preproc_graph);
-    CHGraph::preproc_graph_bottom_up(graph, preproc_graph);
+    std::string file_path = "file.txt";
+    FileFacilities::read_graph(file_path, graph);
     EXPECT_EQ(3+2, 5);
     EXPECT_TRUE(true);
 }
