41965bca99be8538adb58afb4239e452827688d4
Alexander Vardanyan <vardart01@gmail.com>
2025-12-08 23:01:13 +0100

    Add test of successful retrieving for read_destinations

--- src/tst/test_file_facilities.cpp
+++ src/tst/test_file_facilities.cpp
@@ -1,11 +1,24 @@
 #include <gtest/gtest.h>
 #include "file_facilities.hpp"
 #include <string>
+#include <vector>
 
-TEST(TestCaategory2, Test2) {
-    CHGraph::Graph graph;
-    std::string file_path = "file.txt";
-    FileFacilities::read_graph(file_path, graph);
-    EXPECT_EQ(3+2, 5);
-    EXPECT_TRUE(true);
+TEST(ReadDestinationsTests, SuccessfulRetrieving01) {
+    const std::vector<CHGraph::Destination> expeted_destinations = {
+        CHGraph::Destination{.source = 1, .target = 10},
+        CHGraph::Destination{.source = 2, .target = 20},
+        CHGraph::Destination{.source = 89, .target = 56},
+    };
+
+    std::vector<CHGraph::Destination> destinations;
+    const std::string destinations_file_path = "tst/data/test_facilities/destinations_01.txt";
+
+    FileFacilities::read_destinations(destinations_file_path, destinations);
+
+    EXPECT_EQ(expeted_destinations.size(), destinations.size());
+    for(int ind = 0; ind < destinations.size(); ++ind)
+    {
+        EXPECT_EQ(expeted_destinations[ind].source, destinations[ind].source);
+        EXPECT_EQ(expeted_destinations[ind].target, destinations[ind].target);
+    }
 }
